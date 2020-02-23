a39c51eed0ea8be12762d6fb8aa79225bf96e4ce
Jerum Galang <jerumpgalang@gmail.com>
2020-02-23 11:57:30 +0800

    added main.ts

---
+++ main.ts
@@ -0,0 +1,21 @@
+/*interface Point {
+    a:number,
+    b:number
+}
+
+
+let drawPoint = (abc: Point ) => {
+    //...
+    abc.a
+}*/
+
+import { Point } from './point'
+
+
+let point:Point = new Point(1, 2);
+//let x = point.x;
+let x = point.getx();
+
+//point.x = 10;
+point.setx(10);
+point.draw();
